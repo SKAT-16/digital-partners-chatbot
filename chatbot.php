0ba9b218ed3cfef2149523f68f775ff4de232a95
SKAT-16 <nathangetachew536@gmail.com>
2025-03-02 13:53:05 +0300

    Add allow of cors origin policy

--- chatbot.php
+++ chatbot.php
@@ -1,5 +1,9 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
+
 include_once "prompt.php";
 include_once 'config.php';
 
